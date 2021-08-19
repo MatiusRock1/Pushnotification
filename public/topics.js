0f6fdb574d70bb07a772d5d3180d0461c2aab917
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-19 18:01:04 -0500

    correccion de invocadores

--- public/topics.js
+++ public/topics.js
@@ -13,3 +13,33 @@ $('#metallica').click(function(){
             }
     });
 });
+$('#anime').click(function(){
+    var data = { token : token, topic : "anime" };
+    $.ajax({
+            url : 'https://firebase.matius-rock.com/registertokentopic',
+            data : data, 
+            method : 'post', //en este caso
+            dataType : 'json',
+            success : function(response){
+                   //codigo de exito
+            },
+            error: function(error){
+                   //codigo error
+            }
+    });
+});
+$('#manga').click(function(){
+    var data = { token : token, topic : "manga" };
+    $.ajax({
+            url : 'https://firebase.matius-rock.com/registertokentopic',
+            data : data, 
+            method : 'post', //en este caso
+            dataType : 'json',
+            success : function(response){
+                   //codigo de exito
+            },
+            error: function(error){
+                   //codigo error
+            }
+    });
+});
