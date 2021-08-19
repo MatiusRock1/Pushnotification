1d635f75ee828111ccc40cfa33e14a533f73aaba
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-19 16:41:05 -0500

    prueba push register

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const morgan = require ('morgan');
+const path = require('path')
+
+
+const app = express();
+
+//setings
+app.set('port', process.env.PORT|| 3000);
+app.set('views', path.join(__dirname,'views/firebase'));
+app.set('view engine', 'ejs');
+app.use("/", express.static(path.join(__dirname, 'public')));
+//mongose
+
+
+const indexRouters = require('./routers/index');
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use('/',indexRouters);
+
+
+
+app.use(morgan('dev'));
+
+app.listen(app.get('port'),() =>{
+    console.log(`Server on port ${app.get('port')}`);
+})
---
+++ public/index.js
@@ -0,0 +1,6 @@
+importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.8.1/firebase-messaging.js");
+
+
+
+messaging.requestPemission
