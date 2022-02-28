1997e547fd65b0d2ac23dbe6b00868a1fd08cdb9
MatiusRock1 <jorgeluistrujilllo@gmail.com>
2022-02-28 15:33:04 -0500

    creation and send message in topic

--- services/notifications/services/firebase.service.js
+++ services/notifications/services/firebase.service.js
@@ -1,32 +1,65 @@
 
-var adminSDKFirebase = require('firebase-admin');
-var serviceAccount = require("../../../path/to/serviceAccountKey.json");
+const boom = require('@hapi/boom');
+var adminSdkFirebase = require('firebase-admin');
+const { getMessaging } = require('firebase-admin/messaging');
+const serviceAccount = require('../../../path/to/serviceAccountKey.json');
+adminSdkFirebase.initializeApp({
+  credential: adminSdkFirebase.credential.cert(serviceAccount),
+}); 
 
 class firebaseService{
 
 constructor(){
-    adminSDKFirebase.initializeApp({
-        credential: adminSDKFirebase.credential.cert(serviceAccount)
-      });
+  
 }
 
 async registerDeviceinTopic(token,topic){
   console.log(token);
     const registrationTokens = [
         token.token
-      ];      
-      await adminSDKFirebase.messaging().subscribeToTopic(registrationTokens, topic)
+      ];               
+      await adminSdkFirebase.messaging().subscribeToTopic(registrationTokens, topic)
         .then((response) => {          
           console.log('Successfully subscribed to topic:', response);
           return "Successfully subscribed to topic";
         })
         .catch((error) => {
           console.log('Error subscribing to topic:', error);
-          return "Error subscribing to topic";
+          throw boom.badGateway('error al registrar token en topic firebase');
         });  
 }
 
+async sendMessageTopic(topic,title,body){
+  var funcionresponse;
+        const message = {
+            notification: {
+                title: title,
+                body: body
+              },     
+          data: {
+            //volume : "3.21.15",
+            type : "messagetopic"
+          },
+          topic: topic    
+        };        
+        await adminSdkFirebase.messaging().send(message)
+          .then((response) => {
+            console.log('Successfully sent message:', response);
+            funcionresponse = { topic : topic,response };
+          })
+          .catch((error) => {
+            console.log('Error sending message:', error);
+            throw boom.badGateway('error al enviar mensaje a topic')
+          });
+      return funcionresponse;
+    }
+
+    async listMessageFirebase(){
+        
+    
 }
+}
+
 
 module.exports = firebaseService;
 
--- services/notifications/services/message.service.js
+++ services/notifications/services/message.service.js
@@ -1,6 +1,12 @@
 const messageModel = require('../db/models/message.model');
 const { populate } = require('../db/models/message.model');
 const boom = require('@hapi/boom');
+const FirebaseService = require('./firebase.service');
+const serviceFirebase = new FirebaseService();
+const TopicsService = require('./topics.service');
+const { shallowCopyFromList } = require('ejs/lib/utils');
+const serviceTopics = new TopicsService();
+
 
 
 class MessageService{
@@ -10,9 +16,36 @@ constructor(){
 }
 
 async create(data){    
-    console.log(data);
+    const topics = data.target.topics;
+    const targetname= data.target.name; 
+
+    
+    var firebaseTopicsResponse = [];
+    switch(targetname){
+       case 'topic':
+        for (let value of topics){ 
+            console.log(value);
+            const topicName = await serviceTopics.findOne(value);
+            console.log(topicName.name);
+            firebaseTopicsResponse.push(await serviceFirebase.sendMessageTopic(topicName.name,data.name,data.description));
+            value += 1;
+        }
+       break;
+       case 'device':
+        console.log('El kilogramo de naranjas cuesta $0.59.');
+       break;
+        default:
+            throw boom.badGateway("data en target incorrecta")
+        break;
+            
+    }
+    
+    
+    
     const newMessage =new messageModel(data);
-    return newMessage.save();
+    newMessage.save();
+    const response = {result: 0 , send : firebaseTopicsResponse}
+    return response;
 }
     
 
